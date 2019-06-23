af90de8d692904ec121a865df6288eae1035dc5f
Glitch (cavtah) <none>
2019-06-23 01:57:22 +0000

    💄🏨 Updated with Glitch

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to CAVTAH!</h2>
+          <h2>Welcome to CAVTAH?</h2>
         </div>
         <p className="App-intro">
           The start of something wonderful.
