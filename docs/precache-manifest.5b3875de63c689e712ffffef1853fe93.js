f06e69e92d057e29b513ff59a29ae6d0270ff7cd
Glitch (sticky-geese) <none>
2019-06-23 01:36:27 +0000

    🙅💎 Updated with Glitch

---
+++ docs/precache-manifest.5b3875de63c689e712ffffef1853fe93.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a0af9af7cbc6f1a5563a4d31dd66482",
+    "url": "/cavtah/index.html"
+  },
+  {
+    "revision": "82edf79387a9a4eb1433",
+    "url": "/cavtah/static/css/main.4575ae16.chunk.css"
+  },
+  {
+    "revision": "fb0cd64659a78ca24e96",
+    "url": "/cavtah/static/js/2.fc539bf0.chunk.js"
+  },
+  {
+    "revision": "82edf79387a9a4eb1433",
+    "url": "/cavtah/static/js/main.554014ef.chunk.js"
+  },
+  {
+    "revision": "5e90d92539de1a54c7bf",
+    "url": "/cavtah/static/js/runtime~main.995c6e8e.js"
+  },
+  {
+    "revision": "002e82a170a3c76b67f9ee617b507f3e",
+    "url": "/cavtah/static/media/logo.002e82a1.svg"
+  }
+]);
