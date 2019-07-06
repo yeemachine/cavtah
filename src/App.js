7bc40c8d426f55bfaa4bd984cfbd2b9eba2e9124
Glitch (cavtah) <none>
2019-07-06 02:23:08 +0000

    🎀💃 Updated with Glitch

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
+import Grid from './Grid.js'
+import Mission from './Mission.js'
 import SVG from './svg.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-          <SVG data="https://cdn.glitch.com/c8411ee0-b2bb-481b-8835-6b597f185b9c%2FMerode%20Altarpiece-vector.svg?v=1561795513950"/>      
+        <Grid />
+        <Mission />    
       </div>
     );
   }
---
+++ src/Grid.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './Grid.css';
+
+class Grid extends Component {
+  render() {
+    return (
+      <div className="Grid">
+        <div className="Grid-Line"></div>
+        <div className="Grid-Line"></div>
+        <div className="Grid-Line"></div>
+        <div className="Grid-Line"></div>
+        <div className="Grid-Line"></div>
+        <div className="Grid-Line"></div>
+        <div className="Grid-Line"></div>
+        <div className="Grid-Line"></div>
+      </div>
+    );
+  }
+}
+
+export default Grid;
