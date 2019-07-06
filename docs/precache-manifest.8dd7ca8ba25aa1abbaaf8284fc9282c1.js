a7544cc3902f57ee097988ccb994a5241a5e2b9d
Glitch (cavtah) <none>
2019-07-06 04:44:20 +0000

    🕌👑 Updated with Glitch

---
+++ docs/precache-manifest.8dd7ca8ba25aa1abbaaf8284fc9282c1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58c33abe6e805243d33c2a1c65d96a67",
+    "url": "/index.html"
+  },
+  {
+    "revision": "329f85f8ea0858d7088d",
+    "url": "/static/css/main.93ae1516.chunk.css"
+  },
+  {
+    "revision": "2b05e342755723a9ecf7",
+    "url": "/static/js/2.b72722e0.chunk.js"
+  },
+  {
+    "revision": "329f85f8ea0858d7088d",
+    "url": "/static/js/main.873416d9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
