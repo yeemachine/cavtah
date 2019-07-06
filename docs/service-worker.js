df1d4b7aacd90b9a8fd9410e877d7f701af1bbae
Glitch (cavtah) <none>
2019-07-06 04:51:36 +0000

    🐌🐾 Updated with Glitch

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8dd7ca8ba25aa1abbaaf8284fc9282c1.js"
+  "/precache-manifest.b54f2c91369b1df33df1f14252aea5b8.js"
 );
 
 self.addEventListener('message', (event) => {
